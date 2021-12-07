2870e5c34eb56e632905accdfa190f361669261e
Bhishma Dedhia <bhishma.dedhia@gmail.com>
2021-12-07 16:21:19 -0500

    Pretraiber modified

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -5,18 +5,16 @@ import torch.nn as nn
 import os
 import sys
 import numpy as np
-import argparse
-import yaml
 sys.path.append('../models/')
 sys.path.append('../training/')
-sys.path.append('../dataloader/')
+sys.path.append('../data_generator/')
 sys.path.append('../transformers/src/')
 from bert2bert import Bert2BertSynCtrl
 from transformers import BertConfig
 from trainer import PreTrainer 
 from dataloader import PreTrainDataLoader
 
-def run_pretraining(args, num_iters=5e5):
+def run_experiment(args, num_iters=5e5):
 
 	device = torch.device('cuda:0' if torch.cuda.is_available else "cpu")	
 
@@ -25,34 +23,11 @@ def run_pretraining(args, num_iters=5e5):
 
 
 	config = yaml.load(open(args.config,'r'),Loader=yaml.FullLoader)
-	
-	if args.target_index is not None:
-		target_id = args.target_index
-		classes = None 
-	
-	elif exp_name == 'basque':
-		target_id = 16#basque county has index 16
-		classes = None 
-
-	elif exp_name == 'germany':
-		target_id = 6# west germany has index 6
-		classes = None 
-
-	elif exp_name == 'prop99':
-		target_id = 2 #california has index 2
-		classes = None 
-
-	#elif exp_name == 'retail':
-
-	#elif exp_name == 'cricket':
-
 	config_model = BertConfig(hidden_size = config['hidden_size'],
 							num_hidden_layers = config['n_layers'],
 							num_attention_heads = config['n_heads'],
 							intermediate_size = 4*config['hidden_size'],
 							vocab_size = 0,
-							max_position_embeddings = 0,
-							output_hidden_states = True,
 							)
 
 	config_model.add_syn_ctrl_config(K=config['K'],
@@ -62,23 +37,14 @@ def run_pretraining(args, num_iters=5e5):
 									time_range=config['time_range'],
 									seq_range=config['seq_range'],
 									cont_dim=config['cont_dim'],
-									discrete_dim=config['discrete_dim'],
-									classes = classes)
+									discrete_dim=config['discrete_dim'])
 
-	if args.data_transform is not None:
-		lowrank = True
-
-	else:
-		lowrank = False
 	model = Bert2BertSynCtrl(config_model, args.random_seed)
 	model = model.to(device)
-	dataloader = PreTrainDataLoader(args.random_seed,
+	dataloader = PreTrainDataLoader(args.seed,
 									args.datapath,
 									device,
-									config_model,
-									target_id,
-									lowrank_approx = lowrank)
-
+									config_model)
 	optimizer = torch.optim.AdamW(model.parameters(),
 								lr=eval(config['lr']),
 								weight_decay=eval(config['weight_decay']),
@@ -96,8 +62,6 @@ def run_pretraining(args, num_iters=5e5):
 						scheduler
 						)
 
-	print(f'Running training with batch_size {batch_size}')
-
 	trainer.train(int(num_iters),args.checkpoint)
 
 
@@ -112,11 +76,6 @@ def main():
 	parser.add_argument('--config',type=str, default='')
 	parser.add_argument('--op_path',type=str,default='')
 	parser.add_argument('--random_seed',type=int,default=0)
-	parser.add_argument('--target_index',type=int,default=0)
 	parser.add_argument('--checkpoint',type=str,default=None)
-	parser.add_argument('--data_transform',type=str,default=None)
 	args = parser.parse_args()
-	run_pretraining(args)
-
-if __name__ == "__main__":
-    main()
+	run_experiment(args)
