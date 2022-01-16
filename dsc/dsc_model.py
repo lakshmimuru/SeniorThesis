3ab2dc6c186271c3164f96e15e2591cf819897a1
Bhishma_Dedhia <bhishma.dedhia@gmail.com>
2022-01-15 23:19:54 -0500

    bug fix

--- dsc/dsc_model.py
+++ dsc/dsc_model.py
@@ -22,160 +22,160 @@ from generator import Generator
 
 class DSCModel(object):
 
-	def __init__(self,
-				model,
-				config,
-				op_dir,
-				target_id,
-				random_seed,
-				datapath,
-				device,
-				lowrank=False,
-				classes=None):
+    def __init__(self,
+                model,
+                config,
+                op_dir,
+                target_id,
+                random_seed,
+                datapath,
+                device,
+                lowrank=False,
+                classes=None):
 
-		self.model = model
-		self.config = config
-		self.op_dir = op_dir
-		self.target_id = target_id
-		self.random_seed = random_seed
-		self.datapath = datapath
-		self.device = device
-		self.lowrank = lowrank
-		self.classes = classes
+        self.model = model
+        self.config = config
+        self.op_dir = op_dir
+        self.target_id = target_id
+        self.random_seed = random_seed
+        self.datapath = datapath
+        self.device = device
+        self.lowrank = lowrank
+        self.classes = classes
 
-		self.config_model = BertConfig(hidden_size = config['hidden_size'],
-							num_hidden_layers = config['n_layers'],
-							num_attention_heads = config['n_heads'],
-							intermediate_size = 4*config['hidden_size'],
-							vocab_size = 0,
-							max_position_embeddings = 0,
-							output_hidden_states = True,
-							)
+        self.config_model = BertConfig(hidden_size = config['hidden_size'],
+                            num_hidden_layers = config['n_layers'],
+                            num_attention_heads = config['n_heads'],
+                            intermediate_size = 4*config['hidden_size'],
+                            vocab_size = 0,
+                            max_position_embeddings = 0,
+                            output_hidden_states = True,
+                            )
 
-		self.config_model.add_syn_ctrl_config(K=config['K'],
-									pre_int_len=config['pre_int_len'],
-									post_int_len=config['post_int_len'],
-									feature_dim=config['feature_dim'],
-									time_range=config['time_range'],
-									seq_range=config['seq_range'],
-									cont_dim=config['cont_dim'],
-									discrete_dim=config['discrete_dim'],
-									classes = classes)
+        self.config_model.add_syn_ctrl_config(K=config['K'],
+                                    pre_int_len=config['pre_int_len'],
+                                    post_int_len=config['post_int_len'],
+                                    feature_dim=config['feature_dim'],
+                                    time_range=config['time_range'],
+                                    seq_range=config['seq_range'],
+                                    cont_dim=config['cont_dim'],
+                                    discrete_dim=config['discrete_dim'],
+                                    classes = classes)
 
-		self.model = Bert2BertSynCtrl(self.config_model, random_seed)
-		self.model = self.model.to(device)
-		if not(os.path.exists(op_dir)):
-			os.mkdir(op_dir)
-
-
-	def fit(self, interv_time, checkpoint_pretrain = None, pretrain =True):
-
-		if pretrain == True:
-			self.pretrain(checkpoint_pretrain)
-
-		if self.model.Bert2BertSynCtrl.config.encoder.K == self.config['K']:
-
-			print('Modifying K')
-			self.model.config.K+=1
-			self.model.K+=1
-			self.model.Bert2BertSynCtrl.encoder.config.K+=1
-			self.model.Bert2BertSynCtrl.decoder.config.K+=1
-
-		self.finetune(interv_time)
-
-	def predict(self, interv_time):
-
-		generator = Generator(self.model,
-						self.device,
-						self.datapath,
-						self.target_id,
-						interv_time,
-						self.lowrank)
-
-		if self.model.Bert2BertSynCtrl.config.encoder.K == self.config['K']:
-
-			print('Modifying K')
-			self.model.config.K+=1
-			self.model.K+=1
-			self.model.Bert2BertSynCtrl.encoder.config.K+=1
-			self.model.Bert2BertSynCtrl.decoder.config.K+=1
-
-		target_data =  generator.sliding_window_generate()
-
-		return target_data
+        self.model = Bert2BertSynCtrl(self.config_model, random_seed)
+        self.model = self.model.to(device)
+        if not(os.path.exists(op_dir)):
+            os.mkdir(op_dir)
 
 
-	def pretrain(self, checkpoint_pretrain, num_iters=5e4):
+    def fit(self, interv_time, checkpoint_pretrain = None, pretrain =True):
+
+        if pretrain == True:
+            self.pretrain(checkpoint_pretrain)
+
+        if self.model.Bert2BertSynCtrl.config.encoder.K == self.config['K']:
+
+            print('Modifying K')
+            self.model.config.K+=1
+            self.model.K+=1
+            self.model.Bert2BertSynCtrl.encoder.config.K+=1
+            self.model.Bert2BertSynCtrl.decoder.config.K+=1
+
+        self.finetune(interv_time)
+
+    def predict(self, interv_time):
 
 
-		dataloader_pretrain = PreTrainDataLoader(self.random_seed,
-									self.datapath,
-									self.device,
-									self.config_model,
-									self.target_id,
-									lowrank_approx = self.lowrank)
+        if self.model.Bert2BertSynCtrl.config.encoder.K == self.config['K']:
+            print('Modifying K')
+            self.model.config.K+=1
+            self.model.K+=1
+            self.model.Bert2BertSynCtrl.encoder.config.K+=1
+            self.model.Bert2BertSynCtrl.decoder.config.K+=1
+        generator = Generator(self.model,
+                    self.device,
+                    self.datapath,
+                    self.target_id,
+                    interv_time,
+                    self.lowrank)
 
-		optimizer_pretrain = torch.optim.AdamW(self.model.parameters(),
-									lr=eval(self.config['lr']),
-									weight_decay=eval(self.config['weight_decay']),
-									)
+        target_data =  generator.sliding_window_generate()
+       
 
-		warmup_steps = self.config['warmup_steps']
-		scheduler = torch.optim.lr_scheduler.LambdaLR(
-					optimizer_pretrain,
-					lambda steps: min((steps+1)/warmup_steps,1))
-		batch_size = self.config['batch_size']
-		op_path_pretrain = self.op_dir + 'pretrain/'
-		if not(os.path.exists(op_path_pretrain)):
-			os.mkdir(op_path_pretrain)
-
-		trainer_pretrain = Trainer(self.model,
-						optimizer_pretrain,
-						dataloader_pretrain,
-						op_path_pretrain,
-						batch_size,
-						scheduler
-						)
-
-		print('Pretraining model on donor units')
-
-		self.model = trainer_pretrain.train(int(num_iters),checkpoint_pretrain)
+        return target_data
 
 
-	def finetune(self, interv_time, num_iters=1e4):
+    def pretrain(self, checkpoint_pretrain, num_iters=5e4):
 
-		dataloader_finetune = FinetuneDataLoader(self.random_seed,
-									self.datapath,
-									self.device,
-									self.config_model,
-									self.target_id,
-									interv_time,
-									lowrank_approx = self.lowrank)
 
-		optimizer_finetune = torch.optim.AdamW(self.model.parameters(),
-								lr=eval(self.config['lr']),
-								weight_decay=eval(self.config['weight_decay']),
-									)
-		batch_size = self.config['batch_size']
-		op_path_finetune = self.op_dir + 'finetune/'
-		if not(os.path.exists(op_path_finetune)):
-			os.mkdir(op_path_finetune)
-		trainer = Trainer(self.model,
-							optimizer_finetune,
-							dataloader_finetune,
-							op_path_finetune,
-							batch_size
-							)
+        dataloader_pretrain = PreTrainDataLoader(self.random_seed,
+                                    self.datapath,
+                                    self.device,
+                                    self.config_model,
+                                    self.target_id,
+                                    lowrank_approx = self.lowrank)
 
-		print('Fitting model on target unit')
+        optimizer_pretrain = torch.optim.AdamW(self.model.parameters(),
+                                    lr=eval(self.config['lr']),
+                                    weight_decay=eval(self.config['weight_decay']),
+                                    )
 
-		self.model = trainer.train(int(num_iters))
+        warmup_steps = self.config['warmup_steps']
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+                    optimizer_pretrain,
+                    lambda steps: min((steps+1)/warmup_steps,1))
+        batch_size = self.config['batch_size']
+        op_path_pretrain = self.op_dir + 'pretrain/'
+        if not(os.path.exists(op_path_pretrain)):
+            os.mkdir(op_path_pretrain)
 
-	def load_model_from_checkpoint(self,modelpath):
+        trainer_pretrain = Trainer(self.model,
+                        optimizer_pretrain,
+                        dataloader_pretrain,
+                        op_path_pretrain,
+                        batch_size,
+                        scheduler
+                        )
 
-		cp = torch.load(modelpath)
-		state_dict = cp['model_state_dict']
-		self.model.load_state_dict(state_dict)
+        print('Pretraining model on donor units')
+
+        self.model = trainer_pretrain.train(int(num_iters),checkpoint_pretrain)
+
+
+    def finetune(self, interv_time, num_iters=1e4):
+
+        dataloader_finetune = FinetuneDataLoader(self.random_seed,
+                                    self.datapath,
+                                    self.device,
+                                    self.config_model,
+                                    self.target_id,
+                                    interv_time,
+                                    lowrank_approx = self.lowrank)
+
+        optimizer_finetune = torch.optim.AdamW(self.model.parameters(),
+                                lr=eval(self.config['lr']),
+                                weight_decay=eval(self.config['weight_decay']),
+                                    )
+        batch_size = self.config['batch_size']
+        op_path_finetune = self.op_dir + 'finetune/'
+        if not(os.path.exists(op_path_finetune)):
+            os.mkdir(op_path_finetune)
+        trainer = Trainer(self.model,
+                            optimizer_finetune,
+                            dataloader_finetune,
+                            op_path_finetune,
+                            batch_size
+                            )
+
+        print('Fitting model on target unit')
+
+        self.model = trainer.train(int(num_iters))
+
+    def load_model_from_checkpoint(self,modelpath):
+
+        cp = torch.load(modelpath)
+        state_dict = cp['model_state_dict']
+        self.model.load_state_dict(state_dict)
 
 
 
